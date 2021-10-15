bf8394fd50e510ca2ca8af3369f5802cf31c03aa
Naufal <naufal@ubuntu>
2021-10-15 16:24:42 +0530

    adds stuff

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def parse():
 	return username, password, database, hostname, port
 
 
+@app.route('/')
+def index():
+    return render_template('index2.html')
+
+
 @app.route('/login_page_client')
 def login_page_client():
     return render_template('login_page_client.html')
@@ -48,14 +53,14 @@ def login():
                 cred = cursor.fetchall()
                 dbconn.commit()
             except:
-                return redirect(url_for("login_page"))
+                return redirect(url_for("login_page_client"))
 
             if cred[0][2] == psw:
                 session['user'] = email
                 session['first_name'] = cred[0][3]
                 session['last_name'] = cred[0][4]
                 session['id'] = cred[0][0]
-                return redirect(url_for('therapist'))
+                return redirect(url_for('therapist_index'))
             else:
                 return redirect(url_for('login_page'))     
         else:
@@ -78,23 +83,140 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('login_page_client'))
+
+
+@app.route('/register_client_page')
+def register_client_page():
+    return render_template('register.html')
+
+
+@app.route('/register_client', methods=['POST'])
+def register_client():
+    if request.method == 'POST':
+        username, password, database, hostname, port = parse()
+        email = request.form.get("email")
+        psw = request.form.get("psw")
+        psw_repeat = request.form.get("psw_repeat")
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+        cursor = dbconn.cursor()
+        cursor.execute(f"""INSERT INTO client_cred (email, password, status,first_name,last_name) VALUES (%s,%s,%s,%s,%s);""",(email,psw,'empty',first_name,last_name))
+        dbconn.commit()
+        return redirect('login_page_client')
 
 
 @app.route('/user_index')
 def user_index():
-    return render_template('user_index.html', email = session['user'])
+    username, password, database, hostname, port = parse()
+    dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+    cursor = dbconn.cursor()
+    cursor.execute(f"""SELECT * FROM client_cred WHERE email = %s""",[session['user']])
+    client_info = cursor.fetchall()
+    session['firstname'] = client_info[0][3]
+    session['lastname'] = client_info[0][4]
+    for i in client_info[0]:
+        if i == 'empty':
+            session['profile_complete'] = False
+        else:
+            session['profile_complete'] = True
+            session['client_info'] = client_info
+
+    if session['profile_complete']:
+        description = 'We will let you know when a therapist accepts your profile'
+    else:
+        description = 'Please Complete Profile'
+
+    return render_template('user_index.html', email = session['user'], client_info = client_info, description = description, profile_complete = session['profile_complete'])
 
 
 @app.route('/user_profile')
 def user_profile():
-    return render_template('user_profile.html')
+    client_info = session['client_info']
+    return render_template('user_profile.html' ,client_info = client_info)
 
 
 @app.route('/edit_profile')
 def edit_profile():
-    return render_template('edit_profile.html')
+    if 'user' in session:
+        client_info = session['client_info']
+        return render_template('edit_profile.html', client_info = client_info)
+    else:
+        return redirect(url_for('login_page_client'))
 
+
+@app.route('/edit_info', methods=['POST'])
+def edit_info():
+    if request.method == 'POST':
+        phone = request.form.get('phone')
+        age = request.form.get('age')
+        print(age)
+        city = request.form.get('city')
+        occupation = request.form.get('occupation')
+        concerns = request.form.get('concerns')
+        relationship_status = request.form.get('relationship_status')
+        timeperiod = request.form.get('timeperiod')
+        username, password, database, hostname, port = parse()
+        dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+        cursor = dbconn.cursor()
+        cursor.execute(f"""UPDATE client_cred SET phonenumber = %s WHERE email = %s;""",(phone,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET age = %s WHERE email = %s;""",(age,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET city = %s WHERE email = %s;""",(city,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET occupation = %s WHERE email = %s;""",(occupation,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET concerns = %s WHERE email = %s;""",(concerns,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET relationship_status = %s WHERE email = %s;""",(relationship_status,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET timeperiod = %s WHERE email = %s;""",(timeperiod,session['user']))
+        cursor.execute(f"""UPDATE client_cred SET status = %s WHERE email = %s;""",('free',session['user']))
+        cursor.execute(f"""UPDATE client_cred SET therapist = %s WHERE email = %s;""",('free',session['user']))
+        dbconn.commit()
+        return redirect(url_for('user_index'))
+
+
+@app.route('/therapist_index')
+def therapist_index():
+    email = session['user']
+    arr = email.split('@')
+    arr_2 = arr[1].split('.')
+    arr_3 = [arr[0],arr_2[0]]
+    table_name = ''.join(arr_3)
+    session['table_name'] = table_name
+    username, password, database, hostname, port = parse()
+    dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+    cursor = dbconn.cursor()
+    cursor.execute(f"""SELECT * FROM client_cred;""")
+    client_info = cursor.fetchall()
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {session['table_name']} ( id serial PRIMARY KEY, date VARCHAR NOT NULL, status VARCHAR, client VARCHAR);")
+    dbconn.commit()
+    return render_template('therapist_index.html', client_info = client_info, session = session)
+
+
+@app.route('/accepted_clients')
+def accepted_clients():
+    username, password, database, hostname, port = parse()
+    dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+    cursor = dbconn.cursor()
+    cursor.execute(f"""SELECT * FROM {session['table_name']};""")
+    client_info = cursor.fetchall()
+    dbconn.commit()
+    return render_template('therapist_index.html', client_info = client_info, session = session)
+
+
+@app.route('/accept_client/<ref_email>')
+def accept_client(ref_email):
+    username, password, database, hostname, port = parse()
+    arr = ref_email.split('@')
+    arr_2 = arr[1].split('.')
+    arr_3 = [arr[0],arr_2[0]]
+    ref_email_arr = ''.join(arr_3)
+    dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
+    cursor = dbconn.cursor()
+    cursor.execute(f"""UPDATE client_cred SET status = %s WHERE email = %s;""",('accepted',ref_email))
+    cursor.execute(f"""UPDATE client_cred SET therapist = %s WHERE email = %s;""",(session['user'],ref_email))
+    cursor.execute(f"""INSERT INTO {session['table_name']} (date,status,client) VALUES (%s,%s,%s);""",(date.today(),'accepted',ref_email_arr))
+    dbconn.commit()
+
+    return redirect(url_for('therapist_index'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- db.sql
+++ db.sql
@@ -1,7 +1,10 @@
-INSERT INTO client_cred (email, password, first_name, last_name) 
-VALUES (
-    'madhan@dubakoor.com',
-    'Madhan@123',
-    'Madhan',
-    'Menon'
+CREATE TABLE IF NOT EXISTS therapist_cred (
+    id SERIAL PRIMARY KEY NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(18) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL
 );
+
+INSERT INTO therapist_cred (email,password,first_name,last_name) VALUES ('ramu@dubakoor.com','Ramu@123','Ramu','Lal');
+
